1c26ecb59cead4033b15a4ebfef166d86d91dbb9
Kirstyn <kirstyn.jenna@hotmail.co.uk>
2021-10-29 15:16:27 +0000

    Inital turn of tile functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,23 @@
  //Wait for the DOM to finish loading before running game 
- 
         
         function runGame(){
 
         }
 
+        const tiles = document.querySelectorAll('.game-tile');
+
         function turnTileOver(){
+            this.classList.toggle('turnTileOver');
 
         }
+
+        tiles.forEach(tiles => {tiles.addEventListener('click', turnTileOver)
+            
+        });
 
         function checkTileMatch(){
 
         }
 
         function incrementScore(){
-
         }
