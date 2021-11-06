4aae195acbd5751f24d10c58df75ced4991cbfec
Kirstyn <kirstyn.jenna@hotmail.co.uk>
2021-11-06 15:40:03 +0000

    Added gameReady function and updated countdown details to include 'years'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,140 +1,155 @@
 //Wait for the DOM to finish loading before running game 
-	//Actions during game play
-	const tiles = document.querySelectorAll('.game-tile');
-	
+if (document.readyState === 'loading') {
+	document.addEventListener('DOMContentLoaded', gameReady());
+} else {
+	gameReady();
+}
+
+
+const tiles = document.querySelectorAll('.game-tile');
+let lockBoard = true;
+let hasTurnedTileOver = false;
+let firstTile, secondTile;
+
+
+function gameReady() {
+	document.getElementById("countdown").innerHTML = "50";
+	//pop up start modal 
+	//start music
+
+}
+
+
 //Assistance with this code from here: https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown 
-//Actions on game start
+//Initiates countdown timer and game start
 function startGame() {
-    lockBoard = false;
-        var timeleft = 50;
-        var downloadTimer = setInterval(function () {
-            if (timeleft <= 0) {
-                clearInterval(downloadTimer);
-                document.getElementById("countdown").innerHTML = "GAME OVER";
-            } else {
-                document.getElementById("countdown").innerHTML = timeleft;
-            }
-            timeleft -= 1;
-        }, 1000);
+	lockBoard = false;
+	var timeleft = 50;
+	var countdownTimer = setInterval(function () {
+		//if (document.getElementById("score") === (8)) {
+		//stopCountdown();
+		//} else 
+		if (timeleft <= 0) {
+			clearInterval(countdownTimer);
+			document.getElementById("countdown").innerHTML = "GAME OVER";
+		} else {
+			document.getElementById("countdown").innerHTML = timeleft;
+		}
+		timeleft -= 1;
+	}, 1000);
 
-    }
+}
 
-	let hasTurnedTileOver = false;
-	let lockBoard = true;
-	let firstTile, secondTile;
-	
 
-	function turnTileOver() {
-	    if (lockBoard) return;
-	    this.classList.add('turnTileOver');
-	
+function turnTileOver() {
+	if (lockBoard) return;
+	this.classList.add('turnTileOver');
 
-	    if (!hasTurnedTileOver) {
-	        //first click
-	        hasTurnedTileOver = true;
-	        firstTile = this;
-	    } else {
-	        //second click
-	        hasTurnedTileOver = false;
-	        secondTile = this;
+	if (!hasTurnedTileOver) {
+		//first click
+		hasTurnedTileOver = true;
+		firstTile = this;
+	} else {
+		//second click
+		hasTurnedTileOver = false;
+		secondTile = this;
 
-	        checkForMatch()
-	    }
+		checkForMatch()
 	}
-	
+}
 
-	//Check that tiles are a match
-	function checkForMatch() {
-	    if (firstTile.dataset.image === secondTile.dataset.image) {
-	        //if tiles are a match
-	        disableTiles();
-	        incrementScore();
-	        //if all tiles matched 
-	        //stopCountdown()
-	        //if countDowntime stopped, record time on screen
-	    } else {
-	        //if tiles are NOT a match
-	        unturnTiles();
-	    }
+
+//Check that tiles are a match
+function checkForMatch() {
+	if (firstTile.dataset.image === secondTile.dataset.image) {
+		//if tiles are a match
+		disableTiles();
+		incrementScore();
+	} else {
+		//if tiles are NOT a match
+		unturnTiles();
 	}
-	
+}
 
-	//if tiles are a match
-	function disableTiles() {
-	    firstTile.removeEventListener('click', turnTileOver);
-	    secondTile.removeEventListener('click', turnTileOver);
-	}
-	
 
-	//if tiles are NOT a match
-	function unturnTiles() {
-	    lockBoard = true;
-	
+//if tiles are a match
+function disableTiles() {
+	firstTile.removeEventListener('click', turnTileOver);
+	secondTile.removeEventListener('click', turnTileOver);
+}
 
-	    setTimeout(() => {
-	        firstTile.classList.remove('turnTileOver');
-	        secondTile.classList.remove('turnTileOver');
-	        lockBoard = false;
-	    }, 600);
-	}
-	
 
-	function incrementScore() {
-	    let oldScore = parseInt(document.getElementById("score").innerText);
-	    document.getElementById("score").innerText = ++oldScore;
-	}
-	
+//if tiles are NOT a match
+function unturnTiles() {
+	lockBoard = true;
 
-	function stopCountdown() {
-	
+	setTimeout(() => {
+		firstTile.classList.remove('turnTileOver');
+		secondTile.classList.remove('turnTileOver');
+		lockBoard = false;
+	}, 600);
+}
 
-	}
-	
+//Add to the score if tiles match
+function incrementScore() {
+	let oldScore = parseInt(document.getElementById("score").innerText);
+	document.getElementById("score").innerText = ++oldScore;
+}
 
-	function resetBoard() {
-	
 
-	}
-	
+//function stopCountdown() {
 
-	
+//	var timeinterval = clock.prop("countdown");
+//	if (timeinterval) {
+//		clearInterval(timeInterval);
+//	}
+// }
 
-	
 
-	//setTimeout(() => {
-	
+function resetBoard() {
 
-	//}, timeout);
-	//let countdownElement = document.getElementsById('countdown');
-	//var seconds = 100;
-	
 
-	////if (distance < 0) {
-	//   clearInterval(x);
-	//   document.getElementById("countdown").innerHTML = "GAME OVER";
-	// }
-	//}, 1000);
-	
+}
 
-	
 
-	function shuffleTiles () {
-	
+//setTimeout(() => {
 
-	}
-	//function gameready()
-	function gameReady(){
-	
 
-	}
-	//Actions on game start
-	function startCountdown() {
-	
+//}, timeout);
+//let countdownElement = document.getElementsById('countdown');
+//var seconds = 100;
 
-	}
 
-	
+////if (distance < 0) {
+//   clearInterval(x);
+//   document.getElementById("countdown").innerHTML = "GAME OVER";
+// }
+//}, 1000);
 
-	tiles.forEach(tiles => {
-	    tiles.addEventListener('click', turnTileOver);
-	});
+
+
+
+//function shuffleTiles() {
+//	for (let i = this.tilesArray.length - 1; i > 0; 1--) {
+//		let randIndex = Math.floor(Math.random() * (i + 1));
+//		this.tilesArray[randIndex].style.order * i;
+//		this.tilesArray[i].style.order = randIndex;
+//	}
+
+//}
+
+
+
+//Actions on game start
+function startCountdown() {
+
+
+}
+
+
+//var numMatches = 0;
+
+
+tiles.forEach(tiles => {
+	tiles.addEventListener('click', turnTileOver);
+});
